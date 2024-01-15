514b906ccc959f271183dea1356fda8dc663479d
Mohammed Lareb Zafar <mzafar@yugabyte.com>
2024-01-15 19:41:38 +0530

    Initial commit

---
+++ dist/config/index.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PRODUCT_CATEGORIES = void 0;
+exports.PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui_kits",
+        feature: [
+            {
+                name: "Editor picks",
+                href: "#",
+                imageSrc: "/nav/ui-kits/mixed.jpg",
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/ui-kits/blue.jpg",
+            },
+            {
+                name: "Best sellers",
+                href: "#",
+                imageSrc: "/nav/ui-kits/purple.jpg",
+            },
+        ],
+    },
+    {
+        label: "Icons",
+        value: "icons",
+        feature: [
+            {
+                name: "Favorite Icon Picks",
+                href: "#",
+                imageSrc: "/nav/icons/picks.jpg",
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/icons/new.jpg",
+            },
+            {
+                name: "Best selling Icons",
+                href: "#",
+                imageSrc: "/nav/icons/bestsellers.jpg",
+            },
+        ],
+    },
+];
